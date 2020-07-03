1207a1731ac6a2a0ef5b20e083df488765d1cebf
WisnuHarjanta <hwisnu222@gmail.com>
2020-07-03 11:54:11 +0700

    Add file project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    colors: {
+      dark: '0C1C1C',
+      white: 'EFF1E8',
+      darkLight: '374f45',
+      green: '4DAE2C',
+      greenDark: '47a328',
+      grayGreen: '74c69d'
+    }
+  },
+  variants: {},
+  plugins: [],
+}
